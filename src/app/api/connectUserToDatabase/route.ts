7d5e07559d063884f909214c194c439463c18ff9
danpiths <danpiths@gmail.com>
2023-06-13 00:59:06 +0530

    added a check to see if user already exists before registering to the databse

--- src/app/api/connectUserToDatabase/route.ts
+++ src/app/api/connectUserToDatabase/route.ts
@@ -3,6 +3,7 @@ export const runtime = "edge";
 import { db } from "@/db";
 import { users } from "@/db/schema/User";
 import { auth, currentUser } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 export async function GET() {
@@ -10,12 +11,21 @@ export async function GET() {
   if (!userId) {
     return redirect("/sign-in");
   }
-  const user = await currentUser();
-  if (user?.emailAddresses[0].emailAddress) {
+  const user = await db
+    .select({ id: users.id })
+    .from(users)
+    .where(eq(users.id, userId));
+
+  if (user.length > 0) {
+    return;
+  }
+
+  const userToCreate = await currentUser();
+  if (userToCreate?.emailAddresses[0].emailAddress) {
     await db.insert(users).values({
       id: userId,
-      name: `${user?.firstName} ${user?.lastName}`,
-      email: user?.emailAddresses[0].emailAddress,
+      name: `${userToCreate?.firstName} ${userToCreate?.lastName}`,
+      email: userToCreate?.emailAddresses[0].emailAddress,
     });
   }
   return redirect("/");
