59df49cac58d8803b1e16ea797037c494ed619a7
danpiths <danpiths@gmail.com>
2023-06-13 01:01:19 +0530

    added delete user category route

---
+++ src/app/api/deleteUserCategory/route.ts
@@ -0,0 +1,30 @@
+export const runtime = "edge";
+
+import { db } from "@/db";
+import { categories } from "@/db/schema/Category";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { revalidateTag } from "next/cache";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { userId } = auth();
+  if (!userId) {
+    return NextResponse.json(
+      { error: "Not Logged In" },
+      { status: StatusCodes.FORBIDDEN, statusText: ReasonPhrases.FORBIDDEN }
+    );
+  }
+
+  const res: { id: string } = await request.json();
+
+  await db.delete(categories).where(eq(categories.nanoid, res.id));
+
+  revalidateTag("user-categories");
+
+  return NextResponse.json(
+    { data: "Category Deleted" },
+    { status: StatusCodes.OK, statusText: ReasonPhrases.OK }
+  );
+}
