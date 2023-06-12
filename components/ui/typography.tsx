e8fea7f142462a54e70950b73d0c4e41007f73be
danpiths <danpiths@gmail.com>
2023-06-13 01:50:10 +0530

    added a muted type

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -115,3 +115,15 @@ export function InlineCode({
     </code>
   );
 }
+
+export function Muted({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <p className={`text-sm text-muted-foreground ${className}`}>{children}</p>
+  );
+}
