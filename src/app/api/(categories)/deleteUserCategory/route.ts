899005dab3be3310ec5043e8ad003464dbf354a4
danpiths <danpiths@gmail.com>
2023-06-14 01:43:17 +0530

    updated for all todos related to the category to be deleted as well

--- src/app/api/(categories)/deleteUserCategory/route.ts
+++ src/app/api/(categories)/deleteUserCategory/route.ts
@@ -3,6 +3,7 @@ export const runtime = "edge";
 import { DeleteCategoryBody } from "@/components/forms/TodoForm";
 import { db } from "@/db";
 import { categories } from "@/db/schema/Category";
+import { todos } from "@/db/schema/Todo";
 import { auth } from "@clerk/nextjs";
 import { eq } from "drizzle-orm";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
@@ -20,9 +21,11 @@ export async function POST(request: NextRequest) {
 
   const req: DeleteCategoryBody = await request.json();
 
+  await db.delete(todos).where(eq(todos.categoryId, req.id));
   await db.delete(categories).where(eq(categories.nanoid, req.id));
 
   revalidateTag("user-categories");
+  revalidateTag("user-todos");
 
   return NextResponse.json(
     { data: "Category Deleted" },
