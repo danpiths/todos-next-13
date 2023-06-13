f5eb037528d91d4839bf09fc4b34d24e1d1a2bfe
danpiths <danpiths@gmail.com>
2023-06-13 20:46:38 +0530

    added space above footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { P } from "@/ui/typography";
 
 export default function Footer() {
   return (
-    <footer className="w-full border-t py-2">
+    <footer className="mt-5 w-full border-t py-2">
       <P className="text-center">© 2023 All rights reserved</P>
     </footer>
   );
