8b13a1d2a29aa5bf4add1b5e7414feb8ddf90ada
danpiths <danpiths@gmail.com>
2023-06-21 20:43:26 +0530

    made registerUser end point public and added verification for webhook

--- src/app/api/registerUser/route.ts
+++ src/app/api/registerUser/route.ts
@@ -2,16 +2,40 @@ export const runtime = "edge";
 
 import { db } from "@/db";
 import { users } from "@/db/schema/User";
+import { env } from "@/env.mjs";
 import { WebhookEvent } from "@clerk/nextjs/dist/types/server";
+import { IncomingMessage } from "http";
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { buffer } from "micro";
+import { NextResponse } from "next/server";
+import { Webhook } from "svix";
 
-export async function POST(request: Request) {
-  const { evt } = (await request.json()) as { evt: WebhookEvent };
+const secret = env.CLERK_WEBHOOK_USER_CREATED_SECRET;
 
-  if (evt.type === "user.created") {
-    await db.insert(users).values({
-      id: evt.data.id,
-      name: `${evt.data.first_name} ${evt.data.last_name}`,
-      email: evt.data.email_addresses[0].email_address,
-    });
+export async function POST(request: IncomingMessage) {
+  const payload = (await buffer(request)).toString();
+  const headers = request.headers as Record<string, string>;
+
+  const wh = new Webhook(secret);
+
+  let evt;
+
+  try {
+    evt = wh.verify(payload, headers) as WebhookEvent;
+    if (evt.type === "user.created") {
+      await db.insert(users).values({
+        id: evt.data.id,
+        name: `${evt.data.first_name} ${evt.data.last_name}`,
+        email: evt.data.email_addresses[0].email_address,
+      });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Unauthorized, could not verify token" },
+      {
+        status: StatusCodes.UNAUTHORIZED,
+        statusText: ReasonPhrases.UNAUTHORIZED,
+      }
+    );
   }
 }
--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
     DATABASE_PASSWORD: z.string().min(1),
     DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string().min(1),
+    CLERK_WEBHOOK_USER_CREATED_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
@@ -24,6 +25,8 @@ export const env = createEnv({
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
     DATABASE_URL: process.env.DATABASE_URL,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    CLERK_WEBHOOK_USER_CREATED_SECRET:
+      process.env.CLERK_WEBHOOK_USER_CREATED_SECRET,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
