a896d3ba53849094b044fa4e00ed2b3a096a1bd7
danpiths <danpiths@gmail.com>
2023-06-21 13:12:33 +0530

    rendered sign-in/sign-up with components

--- src/app/(auth)/register/[[...register]]/page.tsx
+++ src/app/(auth)/register/[[...register]]/page.tsx
@@ -1,31 +1,11 @@
 export const runtime = "edge";
 
-import { getCurrentScheme } from "@/utils/colorScheme";
-import { SignUp } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import TodoSignUp from "@/components/Auth/TodoSignUp";
 
 export default async function Page() {
-  const scheme = await getCurrentScheme();
-
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignUp
-        appearance={{
-          baseTheme: scheme === "dark" ? dark : undefined,
-          elements: { rootBox: "w-full lg:w-80", card: "w-full lg:w-80" },
-          layout: {
-            socialButtonsPlacement: "bottom",
-            socialButtonsVariant: "iconButton",
-          },
-          variables: {
-            borderRadius: "7px",
-            colorPrimary: scheme === "dark" ? "#EED4CD" : "#1F1A19",
-            colorText: scheme === "dark" ? "#EED4CD" : "#1F1A19",
-            colorBackground: scheme === "dark" ? "#080808" : "#F2F2F2",
-          },
-        }}
-        afterSignUpUrl="/api/connectUserToDatabase"
-      />
+      <TodoSignUp />
     </div>
   );
 }
--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,31 +1,11 @@
 export const runtime = "edge";
 
-import { getCurrentScheme } from "@/utils/colorScheme";
-import { SignIn } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+import TodoSignIn from "@/components/Auth/TodoSignIn";
 
 export default async function Page() {
-  const scheme = await getCurrentScheme();
-
   return (
     <div className="flex flex-1 items-center justify-center">
-      <SignIn
-        appearance={{
-          baseTheme: scheme === "dark" ? dark : undefined,
-          elements: { rootBox: "w-full lg:w-80", card: "w-full lg:w-80" },
-          layout: {
-            socialButtonsPlacement: "bottom",
-            socialButtonsVariant: "iconButton",
-          },
-          variables: {
-            borderRadius: "7px",
-            colorPrimary: scheme === "dark" ? "#EED4CD" : "#1F1A19",
-            colorText: scheme === "dark" ? "#EED4CD" : "#1F1A19",
-            colorBackground: scheme === "dark" ? "#080808" : "#F2F2F2",
-          },
-        }}
-        afterSignUpUrl="/api/connectUserToDatabase"
-      />
+      <TodoSignIn />
     </div>
   );
 }
